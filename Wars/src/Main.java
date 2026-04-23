46bdc4df27fe3155d95a50daac56be6e3328f46a
Gabriel Romanovski Vitorasse <vitorassegabriel@gmail.com>
2026-04-23 16:24:49 -0300

    Edição - Gabriel

--- Wars/src/Main.java
+++ Wars/src/Main.java
@@ -3,12 +3,17 @@ import Wars.Menu;
 
 public class Main {
     public static void main(String[] args) {
+        Oraculo oc01 = new Oraculo();
+        Guerreiro g01 = new Guerreiro();
+        Bolsa myBolsa = new Bolsa();
+        Item item01 = new Item(1, "Arma", false);
+        Item item02 = new Item(2, "Poca", true);
+        
         boolean jogando = true;
+        g01.myBolsa = myBolsa;
         while (jogando) {
             switch (Menu.mostrarMenuJogo()) {
                 case 0 -> {
-                    Oraculo oc01 = new Oraculo();
-                    Guerreiro g01 = new Guerreiro();
                     oc01.definirNome();
                     ReproduzirSom.tocar("../Wars/Midia/adventure.wav");
                     oc01.prologoIntroducao(g01);
@@ -17,9 +22,6 @@ public class Main {
                     oc01.LoadLevel03();
                 }
                 case 1 -> {
-                    Bolsa myBolsa = new Bolsa();
-                    Item item01 = new Item(1, "Arma", false);
-                    Item item02 = new Item(2, "Poca", true);
                     myBolsa.adicionarItem(item01);
                     myBolsa.adicionarItem(item02);
 
